49237cda931e4467c8c24a2ae713c5696a613e2f
ChristineHsieh <christine830228@gmail.com>
2025-02-13 15:57:21 +0800

    redesign WSConn struct

--- websocket/bean.go
+++ websocket/bean.go
@@ -4,19 +4,94 @@ import (
 	"context"
 	"github.com/gofiber/fiber/v2"
 	"reflect"
+	"time"
 
+	"github.com/dennesshen/photon-core-starter/log"
 	"github.com/gofiber/contrib/websocket"
 )
 
-type EndpointAndHandlers struct {
-	endpoint    SocketEndpoint
-	middlewares []fiber.Handler
+type WSConn struct {
+	instance SocketEndpoint
+	conn     *websocket.Conn
+	ctx      context.Context
+	id       string
+	isClosed bool
 }
 
-type WSConn struct {
-	*websocket.Conn
-	ctx context.Context
-	id  string
+func (w *WSConn) onOpen() error {
+	if err := w.instance.OnOpen(w); err != nil {
+		w.instance.OnError(w, err)
+		_ = w.conn.WriteControl(websocket.CloseMessage,
+			websocket.FormatCloseMessage(websocket.CloseUnsupportedData, err.Error()),
+			time.Now().Add(3*time.Second))
+		_ = w.instance.OnClose(websocket.CloseUnsupportedData, err.Error())
+		w.close()
+		return err
+	}
+	return nil
+}
+
+func (w *WSConn) startListen() {
+	defer w.close()
+	for {
+		_, msg, err := w.conn.ReadMessage()
+		if err != nil {
+			return
+		}
+		if err = w.instance.OnMessage(w, string(msg)); err != nil {
+			w.instance.OnError(w, err)
+			_ = w.conn.WriteControl(websocket.CloseMessage,
+				websocket.FormatCloseMessage(websocket.CloseUnsupportedData, err.Error()),
+				time.Now().Add(3*time.Second))
+			_ = w.instance.OnClose(websocket.CloseUnsupportedData, err.Error())
+			return
+		}
+	}
+}
+
+func (w *WSConn) close() {
+	w.isClosed = true
+	if err := w.conn.Close(); err != nil {
+		log.Logger().Error(w.ctx, "close websocket connection error", "error", err)
+	}
+}
+
+func (w *WSConn) Interrupt(err error) {
+	if w.isClosed {
+		return
+	}
+	defer w.close()
+	if err != nil {
+		w.instance.OnError(w, err)
+		_ = w.conn.WriteControl(websocket.CloseMessage,
+			websocket.FormatCloseMessage(websocket.CloseInternalServerErr, err.Error()),
+			time.Now().Add(3*time.Second))
+		return
+	}
+	_ = w.conn.WriteControl(websocket.CloseMessage,
+		websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""),
+		time.Now().Add(3*time.Second))
+}
+
+func (w *WSConn) WriteControl(messageType int, message []byte, deadline time.Time) error {
+	if w.isClosed {
+		return nil
+	}
+	return w.conn.WriteControl(messageType, message, deadline)
+}
+
+func (w *WSConn) WriteMessage(messageType int, data []byte) error {
+	if w.isClosed {
+		return nil
+	}
+	return w.conn.WriteMessage(messageType, data)
+}
+
+func (w *WSConn) WriteJSON(v interface{}) error {
+	if w.isClosed {
+		return nil
+	}
+	return w.conn.WriteJSON(v)
 }
 
 func (w *WSConn) Ctx() context.Context {
@@ -27,7 +102,12 @@ func (w *WSConn) Id() string {
 	return w.id
 }
 
-var enpointAndHandlersMap = make(map[string]EndpointAndHandlers)
+type EndpointAndHandlers struct {
+	endpoint    SocketEndpoint
+	middlewares []fiber.Handler
+}
+
+var endpointAndHandlersMap = make(map[string]EndpointAndHandlers)
 
 type SocketEndpoint interface {
 	OnOpen(conn *WSConn) error
@@ -44,10 +124,10 @@ func RegisterSocketEndpoint(endpoint SocketEndpoint, middlewares ...func(*fiber.
 		panic("websocket endpoint must be a pointer")
 	}
 	endpointPath := endpoint.GetEndpointPath()
-	if _, ok := enpointAndHandlersMap[endpointPath]; ok {
+	if _, ok := endpointAndHandlersMap[endpointPath]; ok {
 		panic("endpoint path repeat: " + endpointPath)
 	}
-	enpointAndHandlersMap[endpointPath] = EndpointAndHandlers{
+	endpointAndHandlersMap[endpointPath] = EndpointAndHandlers{
 		endpoint:    endpoint,
 		middlewares: middlewares,
 	}
--- websocket/websocket.go
+++ websocket/websocket.go
@@ -18,11 +18,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type pair struct {
-	wsConn   *WSConn
-	endpoint *SocketEndpoint
-}
-
 func init() {
 	bean.Autowire(&component)
 }
@@ -31,13 +26,15 @@ var component struct {
 	Server *fiber.App `autowired:"true"`
 }
 
-var wsConnections = make(map[string]pair)
+var wsConnections = make(map[string]*WSConn)
 
 func Shutdown(ctx context.Context) error {
 	log.Logger().Info(ctx, "shutting down websocket server")
-	for _, pair := range wsConnections {
-		(*pair.endpoint).OnClose(websocket.CloseAbnormalClosure, "server shutdown")
-		_ = pair.wsConn.Close()
+	for _, conn := range wsConnections {
+		_ = conn.WriteControl(websocket.CloseMessage,
+			websocket.FormatCloseMessage(websocket.CloseInternalServerErr, "server shutdown"),
+			time.Now().Add(3*time.Second))
+		conn.close()
 	}
 	return nil
 }
@@ -48,7 +45,7 @@ func Start(ctx context.Context) error {
 		path = path + config.Websocket.ContextPath
 	}
 
-	for key, value := range enpointAndHandlersMap {
+	for key, value := range endpointAndHandlersMap {
 		endpointPath := path + key
 		log.Logger().Info(ctx, "Registering websocket endpoint: "+endpointPath)
 
@@ -62,37 +59,28 @@ func Start(ctx context.Context) error {
 }
 
 func getEndpointHandler(endpointAndHandlers EndpointAndHandlers) fiber.Handler {
-	return websocket.New(func(c *websocket.Conn) {
-		userCtx := getContext(c)
-		instance := shallowCopy(endpointAndHandlers.endpoint)
-		conn := &WSConn{Conn: c, ctx: userCtx, id: generateId(c)}
-		wsConnections[conn.Id()] = pair{wsConn: conn, endpoint: &instance}
-		conn.SetCloseHandler(instance.OnClose)
-		conn.SetPingHandler(instance.OnPing)
-		conn.SetPongHandler(instance.OnPong)
 
-		if err := instance.OnOpen(conn); err != nil {
-			closeProcessWithErr(conn, &instance, err)
+	return websocket.New(func(c *websocket.Conn) {
+
+		instance := shallowCopy(endpointAndHandlers.endpoint)
+		c.SetCloseHandler(instance.OnClose)
+		c.SetPingHandler(instance.OnPing)
+		c.SetPongHandler(instance.OnPong)
+		conn := &WSConn{conn: c, ctx: context.Background(), id: generateId(c), instance: instance}
+
+		wsConnections[conn.Id()] = conn
+		defer delete(wsConnections, conn.Id())
+
+		if err := conn.onOpen(); err != nil {
 			return
 		}
+
 		startPingPong(conn)
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
-				if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
-					closeProcess(conn, &instance)
-					return
-				}
-				closeProcessWithErr(conn, &instance, err)
-				return
-			}
-			if err = instance.OnMessage(conn, string(msg)); err != nil {
-				closeProcessWithErr(conn, &instance, err)
-				return
-			}
-		}
+		conn.startListen()
+
 	})
 }
+
 func generateId(c *websocket.Conn) string {
 	uuid := uuid.New()
 	headers, _ := c.Locals("headers").(map[string][]string)
@@ -101,36 +89,6 @@ func generateId(c *websocket.Conn) string {
 	key := fmt.Sprintf("%s%s%d%v", uuid.String(), ip, timestamp, headers)
 	sum256 := sha256.Sum256([]byte(key))
 	return hex.EncodeToString(sum256[:])
-}
-
-func getContext(c *websocket.Conn) context.Context {
-	var userCtx context.Context
-	if fiberCtx, ok := c.Locals("user_context").(context.Context); ok {
-		userCtx = fiberCtx
-	} else {
-		panic("user_context not found")
-	}
-	return userCtx
-}
-
-func closeProcessWithErr(conn *WSConn, s *SocketEndpoint, err error) {
-	_ = conn.WriteControl(websocket.CloseMessage,
-		websocket.FormatCloseMessage(websocket.CloseUnsupportedData, err.Error()),
-		time.Now().Add(3*time.Second))
-	(*s).OnError(conn, err)
-	if err := (*s).OnClose(websocket.CloseUnsupportedData, err.Error()); err != nil {
-		log.Logger().Error(conn.Ctx(), "Close websocket error: "+err.Error())
-	}
-	conn.Close()
-	delete(wsConnections, conn.Id())
-}
-
-func closeProcess(conn *WSConn, s *SocketEndpoint) {
-	if err := (*s).OnClose(websocket.CloseNormalClosure, ""); err != nil {
-		log.Logger().Error(conn.Ctx(), "Close websocket error: "+err.Error())
-	}
-	conn.Close()
-	delete(wsConnections, conn.Id())
 }
 
 func startPingPong(wsConn *WSConn) {
@@ -173,13 +131,12 @@ func shallowCopy[T any](t T) T {
 func SocketUpgradeHandler(ctx *fiber.Ctx) error {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Logger().Error(ctx.UserContext(), "SocketUpgradeHandler panic", "error", r)
+			log.Logger().Error(ctx.UserContext(), "WebsocketHandler panic", "error", r)
 		}
 	}()
 	if !websocket.IsWebSocketUpgrade(ctx) {
 		return fiber.ErrUpgradeRequired
 	}
 	ctx.Locals("headers", ctx.GetReqHeaders())
-	ctx.Locals("user_context", ctx.UserContext())
 	return ctx.Next()
 }
